60102a7cb906ce8a28b8190a171e58045449bb8d
harshit0414 <144409946+harshit0414@users.noreply.github.com>
2026-02-24 18:40:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobportal-frontend-one-rho.vercel.app/',
+    origin:'https://jobportal-frontend-one-rho.vercel.app',
     credentials:true
 }
 
