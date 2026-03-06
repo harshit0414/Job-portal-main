4740bfab6545600eff6f3eb99a3ab8aa7c5d8e99
harshit0414 <144409946+harshit0414@users.noreply.github.com>
2026-03-06 17:59:01 +0530

    Enhance error handling in MongoDB connection
    Added process.exit(1) on connection error to terminate the app.

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('mongodb connected successfully');
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
 export default connectDB;
