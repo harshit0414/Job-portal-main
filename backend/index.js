c4c418477b3d3d316905cf335dec8c73e9704c84
harshit0414 <144409946+harshit0414@users.noreply.github.com>
2026-03-06 18:41:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import dotenv from "dotenv";import connectDB from "./utils/db.js";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
@@ -16,13 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
+app.use(cors({
+  origin: "https://job-portal-frontend.onrender.com",
+  credentials: true
+}));
 
-const corsOptions = {
-    origin:'https://job-portal-3nxy.onrender.com',
-    credentials:true
-}
-
-app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 8000;
 
@@ -41,6 +40,7 @@ app.use("/api/v1/application", applicationRoute);
 
 
 export default app;
+
 
 
 
